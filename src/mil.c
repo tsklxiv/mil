1d4fee6b5106f21fa3c0fdc90f80b086da6fa5cd
Dang Hoang Tuan <danghoangtuan526@proton.me>
2022-08-06 15:38:23 +0700

    Update the help section a little bit

--- src/mil.c
+++ src/mil.c
@@ -151,6 +151,8 @@ char* readsrc(const char* filename) {
 // == Main ==
 int main(int argc, char* argv[]) {
   if (argc > 2) {
+    printf("Mil is a simple stack-based programming language written in C.\n");
+    printf("Version %s", MIL_VERSION);
     printf("Usage: mil <src>\n");
   } else if (argc == 2) {
     code = readsrc(argv[1]);
